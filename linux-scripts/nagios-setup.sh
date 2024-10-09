fae09dfd807951d431730b8dcbafd69d0c061de4
Aaron Goldsmith <goldsmithaaron@gmail.com>
2024-10-09 15:26:38 -0700

    DRY up fetch code and add nagios server to xinetd's only_from

--- linux-scripts/nagios-setup.sh
+++ linux-scripts/nagios-setup.sh
@@ -13,6 +13,40 @@ else
   echo "Did not receive an expected role (bastion|proxy|webserver-a|webserver-b), bailing"
   exit 1
 fi
+if [[ "$server_role" =~ (proxy|webserver-a|webserver-b) ]] ; then
+  if [ -z "$2" ] ; then
+    echo "Expected to be provided a nagios server IP address to enable NRPE"
+    exit 1
+  fi
+  nagios_server="$2"
+fi
+
+# Set desired version variables
+nagios_version="4.5.6"
+plugins_version="2.4.12"
+nrpe_version="4.1.1"
+
+# An attempt to DRY up the fetch and unpack step that we are doing in multiple places
+# * name: nagios, nagios-plugins, nrpe
+# * GitHub releases path details: because this pathing is not identical between all the tarballs that will be downloaded, examples:
+#     NagiosEnterprises/nagioscore/releases/download/nagios,
+#     NagiosEnterprises/nrpe/releases/download/nrpe,
+#     nagios-plugins/nagios-plugins/releases/download/release
+# * version: semver like "4.1.1"
+function fetch_and_unpack_tarball() {
+  local name="$1"
+  local github_path="$2"
+  local version="$3"
+  local url_prefix="https://githubcom/${github_path}-${version}"
+  local tarball="${name}-${version}.tar.gz"
+
+  # Clean up previous downloads and unpacks for safety
+  sudo rm -rf "/tmp/${name}*"
+  # Fetch
+  wget "${url_prefix}/${tarball}" -P /tmp
+  # Unpack
+  tar xzf "/tmp/${tarball}" -C /tmp
+}
 
 # Only install the nagios server and plugins on the bastion host
 # Following https://support.nagios.com/kb/article/nagios-core-installing-nagios-core-from-source-96.html#Ubuntu install process
@@ -22,17 +56,11 @@ if [[ $server_role == "bastion" ]] && ! sudo systemctl status nagios ; then
   sudo apt-get install -y autoconf gcc libc6 make wget unzip apache2 php libapache2-mod-php7.4 libgd-dev
   sudo apt-get install openssl libssl-dev
 
-  # Download current release and unpack the tarball to a temp directory
-  current_release="4.5.6"
-  url_prefix="https://github.com/NagiosEnterprises/nagioscore/releases/download/nagios-${current_release}"
-  tarball="nagios-${current_release}.tar.gz"
-  # Clean previous downloads and unpacks for safety (have to use sudo because install steps run with sudo...)
-  sudo rm -rf "/tmp/nagios*"
-  wget "${url_prefix}/${tarball}" -P /tmp
-  tar xzf "/tmp/${tarball}" -C /tmp
+  # Download
+  fetch_and_unpack_tarball "nagios" "NagiosEnterprises/nagioscore/releases/download/nagios" "$nagios_version"
 
   # Run install steps for nagios core
-  pushd "/tmp/nagios-${current_release}"
+  pushd "/tmp/nagios-${nagios_version}"
   # Compile code
   sudo ./configure --with-httpd-conf=/etc/apache2/sites-enabled
   sudo make all
@@ -73,22 +101,28 @@ if [ ! -f /usr/local/nagios/libexec/check_wave ] ; then
   # Prereqs
   sudo apt-get install -y autoconf gcc libc6 libmcrypt-dev make libssl-dev wget bc gawk dc build-essential snmp libnet-snmp-perl gettext
   # Download
-  current_release="2.4.12"
-  url_prefix="https://github.com/nagios-plugins/nagios-plugins/releases/download/release-${current_release}"
-  tarball="nagios-plugins-${current_release}.tar.gz"
-  # Clean previous downloads and unpacks for safety
-  sudo rm -rf "/tmp/nagios-plugins*"
-  wget "${url_prefix}/${tarball}" -P /tmp
-  tar xzf "/tmp/${tarball}" -C /tmp
+  fetch_and_unpack_tarball "nagios-plugins" "nagios-plugins/nagios-plugins/releases/download/release" "$plugins_version"
 
   # Run install steps for nagios-plugins
-  pushd "/tmp/nagios-plugins-${current_release}"
+  pushd "/tmp/nagios-plugins-${plugins_version}"
   # Apparently the ./tools/setup step is only necessary if you download the latest from the master branch
   # https://support.nagios.com/forum/viewtopic.php?t=57997 and https://github.com/nagios-plugins/nagios-plugins/issues/545
   #sudo ./tools/setup
   sudo ./configure
   sudo make
   sudo make install
+  popd # return to directory user was operating in before
+fi
+
+# If on the nagios server side and do not have the check_nrpe check present, install it
+if [[ "$server_role" == "bastion" ]] && [ ! -f /usr/local/nagios/libexec/check_nrpe ] ; then
+  # Download
+  fetch_and_unpack_tarball "nrpe" "NagiosEnterprises/nrpe/releases/download/nrpe" "$nrpe_version"
+
+  pushd "/tmp/nrpe-${nrpe_version}"
+  ./configure
+  make check_nrpe
+  make install-plugin
   popd # return to directory user was operating in before
 fi
 
@@ -108,17 +142,10 @@ if [[ "$server_role" =~ (proxy|webserver-a|webserver-b) ]] && ! sudo systemctl s
   # Prereqs
   sudo apt-get install xinetd
   # Download
-  current_release="4.1.1"
-  # https://github.com/NagiosEnterprises/nrpe/releases/download/nrpe-4.1.1/nrpe-4.1.1.tar.gz
-  url_prefix="https://github.com/NagiosEnterprises/nrpe/releases/download/nrpe-${current_release}"
-  tarball="nrpe-${current_release}.tar.gz"
-  # Clean previous downloads and upacks for safety
-  sudo rm -rf "/tmp/nrpe*"
-  wget "${url_prefix}/${tarball}" -P /tmp
-  tar xzf "/tmp/${tarball}" -C /tmp
+  fetch_and_unpack_tarball "nrpe" "NagiosEnterprises/nrpe/releases/download/nrpe" "$nrpe_version"
 
   # Run install steps for nagios-plugins
-  pushd "/tmp/nrpe-${current_release}"
+  pushd "/tmp/nrpe-${nrpe_version}"
   sudo ./configure
   sudo make all
   sudo make install-groups-users
@@ -127,6 +154,9 @@ if [[ "$server_role" =~ (proxy|webserver-a|webserver-b) ]] && ! sudo systemctl s
   sudo make install-inetd
   sudo make install-init
   popd # return to directory user was operating in before
+
+  # Ensure xinetd will receive traffic from the nagios server passed into the script
+  sudo sed -i "s/only_from       = 127.0.0.1 ::1$/only_from       = 127.0.0.1 ::1 $nagios_server/" /etc/xinetd.d/nrpe
 
   # Complete installation with a reload and starting of nrpe
   sudo systemctl reload xinetd
